4fe865d08bf8d595698058c7c56e35e384792224
Bryan Russell <84806406+BryanRussell523@users.noreply.github.com>
2024-05-16 14:34:49 +0100

    Update UserchatActivity.java

--- app/src/main/java/com/example/oya/UserchatActivity.java
+++ app/src/main/java/com/example/oya/UserchatActivity.java
@@ -2,11 +2,16 @@ package com.example.oya;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.graphics.drawable.Drawable;
 import android.media.Image;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Message;
+import android.provider.MediaStore;
+import android.provider.OpenableColumns;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
@@ -15,6 +20,7 @@ import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
+import android.webkit.MimeTypeMap;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -28,6 +34,8 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -53,12 +61,17 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.ktx.Firebase;
 
+import android.Manifest;
+
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class UserchatActivity extends AppCompatActivity implements PopupMenu.OnMenuItemClickListener {
 
     private String chatId;
+    private String otherUserPhone;
+    private String otherUserProfileImageUrl;
     private Toolbar toolbar;
     private TextView nameofuser, Online, Offline;
     private ImageView back, userImage, extra, sendVoiceMessage, sendMessage;
@@ -79,6 +92,12 @@ public class UserchatActivity extends AppCompatActivity implements PopupMenu.OnM
     private DatabaseReference userChatsRef;
 
 
+    private static final int PICK_DOCUMENT_REQUEST = 1;
+
+    private static final int PICK_IMAGE_REQUEST = 2;
+    private static final int PICK_VIDEO_REQUEST  = 3;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -86,45 +105,9 @@ public class UserchatActivity extends AppCompatActivity implements PopupMenu.OnM
         // Set status bar color to white
         setStatusBarColorToWhite();
 
-
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("");
-
-
-        //Retrieve the chat details from the intent
-
-
-        FirebaseAuth mAuth = FirebaseAuth.getInstance();
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-
-        //Initialize user_chats reference
-
-        userChatsRef = FirebaseDatabase.getInstance().getReference().child("user_chats");
-
-        if (currentUser != null) {
-            currentUserId = currentUser.getUid();
-            otherUserId = getIntent().getStringExtra("USER_ID");
-
-            //Generate Chat ID
-            chatId = generateChatId(currentUserId, otherUserId);
-            messageRef = FirebaseDatabase.getInstance().getReference().child("chats").child(chatId).child("messages");
-
-            //After generating chatId, create and populate user_chats node
-            updateUserChatsNode(currentUserId, otherUserId, chatId);
-            updateUserChatsNode(otherUserId, currentUserId, chatId);
-
-        } else {
-            // Handle the case where current user is null
-            Log.e("UserchatActivity", "Current user is null");
-            Toast.makeText(this, "User not authenticated", Toast.LENGTH_SHORT).show();
-            finish(); // Close the activity if user is not authenticated
-        }
-        initializeRecyclerView();
-        loadMessages();
-
-
-        String senderId = FirebaseAuth.getInstance().getCurrentUser().getUid();
 
         nameofuser = findViewById(R.id.nameofuser);
         userImage = findViewById(R.id.userImage);
@@ -148,10 +131,69 @@ public class UserchatActivity extends AppCompatActivity implements PopupMenu.OnM
         sendVoiceMessage = findViewById(R.id.sendVoiceMessage);
         sendMessage = findViewById(R.id.sendMessage);
 
+        //Retrieve intent extras from the recent chat list
+        Intent RecentChatIntent = getIntent();
+        if (RecentChatIntent != null) {
+            chatId = RecentChatIntent.getStringExtra("CHAT_ID");
+            otherUserId = RecentChatIntent.getStringExtra("OTHER_USER_ID");
+            otherUserPhone = RecentChatIntent.getStringExtra("OTHER_USER_PHONE");
+            otherUserProfileImageUrl = RecentChatIntent.getStringExtra("OTHER_USER_IMAGE_URL");
+
+            //Load data into views
+            if (otherUserPhone != null && otherUserProfileImageUrl != null) {
+                nameofuser.setText(otherUserPhone);
+                RequestOptions requestOptions = new RequestOptions()
+                        .placeholder(R.drawable.defaultprofilepic)
+                        .error(R.drawable.defaultprofilepic)
+                        .transform(new CircleCrop());
+                Glide.with(this)
+                        .load(otherUserProfileImageUrl)
+                        .apply(requestOptions)
+                        .into(userImage);
+            }
+        }
+        //Retrieve the chat details from the intent
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        //Initialize user_chats reference
+        userChatsRef = FirebaseDatabase.getInstance().getReference().child("user_chats");
+
+        if (currentUser != null) {
+            currentUserId = currentUser.getUid();
+            otherUserId = getIntent().getStringExtra("OTHER_USER_ID"); // Corrected to "OTHER_USER_ID"
+            if (otherUserId != null) {
+                //Generate Chat ID
+                chatId = generateChatId(currentUserId, otherUserId);
+                messageRef = FirebaseDatabase.getInstance().getReference().child("chats").child(chatId).child("messages");
+
+                //After generating chatId, create and populate user_chats node
+                updateUserChatsNode(currentUserId, otherUserId, chatId);
+                updateUserChatsNode(otherUserId, currentUserId, chatId);
+            } else {
+                // Handle the case where otherUserId is null
+                Log.e("UserchatActivity", "Other user ID is null");
+                Toast.makeText(this, "Other user data not found", Toast.LENGTH_SHORT).show();
+                finish(); // Close the activity if other user data is not found
+            }
+        } else {
+            // Handle the case where current user is null
+            Log.e("UserchatActivity", "Current user is null");
+            Toast.makeText(this, "User not authenticated", Toast.LENGTH_SHORT).show();
+            finish(); // Close the activity if user is not authenticated
+        }
+
+        initializeRecyclerView();
+        loadMessages();
+
+
+        String senderId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+
         //Intent from select user activity
         Intent intent = getIntent();
         if (intent != null) {
-            String userId = intent.getStringExtra("USER_ID");
+            String userId = intent.getStringExtra("OTHER_USER_ID");
             String userPhone = intent.getStringExtra("USER_PHONE");
             String userProfileImageUrl = intent.getStringExtra("USER_IMAGE_URL");
 
@@ -220,10 +262,8 @@ public class UserchatActivity extends AppCompatActivity implements PopupMenu.OnM
             public void onClick(View v) {
                 // Start UserProfileView activity with other user's details
                 startActivityWithUserProfileDetails(otherUserId);
-
             }
-        }
-        );
+        });
 
         nameofuser.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -231,6 +271,23 @@ public class UserchatActivity extends AppCompatActivity implements PopupMenu.OnM
                 // Start UserProfileView activity with other user's details
 
                 startActivityWithUserProfileDetails(otherUserId);
+            }
+        });
+        //Extra menu Click Listeners
+        document.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDocumentPicker();
+            }
+        });
+        gallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Open the device gallery
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                intent.setType("image/*");
+                startActivityForResult(intent, PICK_IMAGE_REQUEST);
+
             }
         });
     }
@@ -256,12 +313,14 @@ public class UserchatActivity extends AppCompatActivity implements PopupMenu.OnM
                     Toast.makeText(UserchatActivity.this, "User data not found", Toast.LENGTH_SHORT).show();
                 }
             }
+
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
                 // Handle onCancelled event
             }
         });
     }
+
     private void initializeRecyclerView() {
         recyclerview = findViewById(R.id.recyclerview);
         recyclerview.setNestedScrollingEnabled(false);
@@ -412,19 +471,155 @@ public class UserchatActivity extends AppCompatActivity implements PopupMenu.OnM
         }
     }
 
-    @Override
-    public void onBackPressed() {
-        Intent intent = new Intent(UserchatActivity.this, homeActivity.class);
-        startActivity(intent);
-        finish();
+    private void openDocumentPicker() {
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setType("*/*");// Allow all file types
+        startActivityForResult(intent, PICK_DOCUMENT_REQUEST);
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_DOCUMENT_REQUEST && resultCode == RESULT_OK && data != null) {
+            // Handle the selected document
+            Uri selectedDocumentUri = data.getData();
+            if (selectedDocumentUri != null) {
+                // Send the document message
+                sendDocument(selectedDocumentUri);
+            }
+        } else if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null) {
+            // Handle the selected image from the device gallery
+            Uri selectedImageUri = data.getData();
+            if (selectedImageUri != null) {
+                // Send the image message
+                sendImage(selectedImageUri);
+            }
+        } else if (requestCode == PICK_VIDEO_REQUEST && resultCode == RESULT_OK && data != null) {
+            //Handle the selected video from the device gallery
+            Uri selectedVideoUri = data.getData();
+            if (selectedVideoUri != null) {
+                //send teh video message
+                sendVideo(selectedVideoUri);
+            }
+        }
+    }
+    private void sendDocument(Uri selectedDocumentUri) {
+        String messageId = messageRef.push().getKey();
+        String documentName = getDocumentName(selectedDocumentUri);
+        String documentType = getDocumentType(selectedDocumentUri);
+        String documentUrl = selectedDocumentUri.toString();
+
+        Log.d("UserchatActivity787", "Document URL: " + documentUrl);
+
+        long timestamp = System.currentTimeMillis();
+        MessageObject message = new MessageObject(messageId, currentUserId, otherUserId, "", timestamp, documentName, documentType, documentUrl, "", "","");
+
+        messageRef.child(messageId).setValue(message)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        //Document message sent successfully
+                        //Add the document message to the recyclerView
+                        messageList.add(message);
+                        messageAdapter.notifyItemInserted(messageList.size() - 1);
+                        recyclerview.scrollToPosition(messageList.size() - 1);
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+
+                    }
+                });
+    }
+
+    private String getDocumentName(Uri documentUri) {
+        Cursor cursor = getContentResolver().query(documentUri, null, null, null, null);
+        String displayName = null;
+        try {
+            if (cursor != null && cursor.moveToFirst()) {
+                displayName = cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME));
+            }
+        } finally {
+            if (cursor != null) {
+                cursor.close();
+            }
+        }
+        return displayName != null ? displayName : "";
+    }
+
+    private String getDocumentType(Uri documentUri) {
+        String fileExtension = MimeTypeMap.getFileExtensionFromUrl(documentUri.toString());
+        String mimeType;
+
+        if (fileExtension != null && fileExtension.equalsIgnoreCase("docx")) {
+            // If the file extension is .docx, return the specific MIME type for Word documents
+            mimeType = "application/vnd.openxmlformats-officedocument.wordprocessingml.document";
+        } else {
+            // Otherwise, use ContentResolver to get the MIME type
+            mimeType = getContentResolver().getType(documentUri);
+        }
+        return mimeType != null ? mimeType : "";
+    }
+
+    public void sendImage(Uri selectedImageUri) {
+        String messageId = messageRef.push().getKey();
+        String imageUrl = selectedImageUri.toString();
+        long timestamp = System.currentTimeMillis();
+        MessageObject message = new MessageObject(messageId, currentUserId, otherUserId, "", timestamp, "", "", "", imageUrl, "","");
+        messageRef.child(messageId).setValue(message)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        //Image message sent successfully
+                        //Add the image message to the recyclerView
+                        messageList.add(message);
+                        messageAdapter.notifyItemInserted(messageList.size()-1);
+                        recyclerview.scrollToPosition(messageList.size() -1);
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        // Handle failure to send image
+                        Log.e("UserchatActivity", "Failed to send image: " + e.getMessage());
+                        Toast.makeText(UserchatActivity.this, "Failed to send image", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+    private void sendVideo(Uri selectedVideoUrl) {
+        String messageId = messageRef.push().getKey();
+        String videoUrl = selectedVideoUrl.toString();
+        long timestamp = System.currentTimeMillis();
+
+        MessageObject message = new MessageObject(messageId, currentUserId, otherUserId, "", timestamp, "", "", "", "", videoUrl, "");
+        messageRef.child(messageId).setValue(message)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        //Video message sent successfully
+                        //add the video message to the recyclerView
+                        messageList.add(message);
+                        messageAdapter.notifyItemInserted(messageList.size() - 1);
+                        recyclerview.scrollToPosition(messageList.size() - 1);
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        // Handle failure to send video
+                        Log.e("UserchatActivity", "Failed to send video: " + e.getMessage());
+                        Toast.makeText(UserchatActivity.this, "Failed to send video", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
     private void sendMessage() {
         String messageText = editTextMessage.getText().toString().trim();
         if (!messageText.isEmpty()) {
             long timestamp = System.currentTimeMillis();
             String messageId = messageRef.push().getKey();
-            MessageObject message = new MessageObject(messageId, currentUserId, otherUserId, messageText, timestamp);
+            MessageObject message = new MessageObject(messageId, currentUserId, otherUserId, messageText, timestamp, "", "", "", "", "","");
             messageRef.child(messageId).setValue(message)
                     .addOnSuccessListener(new OnSuccessListener<Void>() {
                         @Override
@@ -441,5 +636,12 @@ public class UserchatActivity extends AppCompatActivity implements PopupMenu.OnM
                         }
                     });
         }
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent(UserchatActivity.this, homeActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
